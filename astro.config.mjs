be2a11615f8f6519f103eaed46aa292c0acc2a35
imRami <ramiritoriv@gmail.com>
2026-05-31 04:42:32 -0300

    instalando astro sanity oficial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import { sanity } from 'astro-sanity'; // <-- Corregido acá
 
-// https://astro.build
 export default defineConfig({
-  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: 'y6hoafve', // Tu ID único de Sanity
+      dataset: 'production',
+      useCdn: false,
+      apiVersion: '2024-03-12',
+    })
+  ]
 });
