ee77600ea62bc223b5e048ef5c8742b8ab145358
imRami <ramiritoriv@gmail.com>
2026-05-31 04:53:12 -0300

    restaurando configuracion nativa de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { sanity } from 'astro-sanity';
 
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    sanity({
-      projectId: 'y6hoafve', // Tu ID único de Sanity
-      dataset: 'production',
-      useCdn: false,
-      apiVersion: '2024-03-12',
-    })
-  ]
+  integrations: [tailwind()]
 });
